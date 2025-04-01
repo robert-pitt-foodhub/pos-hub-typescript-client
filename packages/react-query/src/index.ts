36805faa2827d288aafb3b6895869c2eda99ab9a
robertpitt <robertpitt1988@gmail.com>
2025-04-01 13:56:46 +0100

    Updates

--- packages/react-query/src/index.ts
+++ packages/react-query/src/index.ts
@@ -1,2 +1,9 @@
 export * from "./generated/index.schemas";
 export * from "./generated/index";
+
+import { useGetV1ResellersResellerId } from "./generated/index";
+
+const resellerLocations = useGetV1ResellersResellerId("resellerId", {
+  axios: {},
+  query: {},
+});
